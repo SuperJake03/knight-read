4551a96e10e69ca3dffd950e8c3672492eec4c48
Superjake03 <jacoblopez0308@gmail.com>
2026-02-27 21:45:31 -0600

    parsing foundations

---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("hello knight readers!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/parse_epub.py
@@ -0,0 +1,8 @@
+import os
+
+import ebooklib
+from ebooklib import epub
+
+
+def parse_epub(book_path):
+    pass
