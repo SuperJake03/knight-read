bad88825b15f3f5440bda68bfcf1976b6b09cf0d
Superjake03 <jacoblopez0308@gmail.com>
2026-03-06 20:58:57 -0600

    changes

--- backend/epub_parser.py
+++ backend/epub_parser.py
@@ -6,7 +6,7 @@ def parse_upload(file_path):
     book = epub.read_epub(file_path)
 
     title_meta = book.get_metadata("DC", "title")
-    title = title_meta[0][0] if title_meta else "Unkown Title"
+    title = title_meta[0][0] if title_meta else "Unknown Title"
 
     author_meta = book.get_metadata("DC", "creator")
     if len(author_meta) == 1:
@@ -14,7 +14,7 @@ def parse_upload(file_path):
     elif len(author_meta) > 1:
         author = ", ".join([a[0] for a in author_meta])
     else:
-        author = "Unkown Author"
+        author = "Unknown Author"
 
     """
     TODO: Finish this function
